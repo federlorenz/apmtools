d8f5eb2c8478ccdf123b7aaae58821c2a0a04031
Federico Lorenzetti <lorenzetti.federico@gmail.com>
2026-02-24 21:47:33 +0000

    ijbnio

--- src/apmtools/data_processing.py
+++ src/apmtools/data_processing.py
@@ -922,7 +922,7 @@ def sum_processing(zipname,processor_name = [],return_data=False,return_csv=True
                         metrics[name] = pd.read_json(gzip.open(BytesIO(archive.read(i)), mode="rt"),convert_dates=False).set_index("timestamp")
                         metrics[name].index = metrics[name].index.map(
                             to_datetime_metrics)
-                metrics[name].index.names = "Datetime"
+                metrics[name].index.rename("Datetime",inplace=True)
                 to_be_dropped = list(set(metrics[name].columns).intersection(set(["channel","sensor_type_id","created_at","download_time","start_time","mission_id"])))
                 metrics[name].drop(
                     axis=1, labels=to_be_dropped, inplace=True)
