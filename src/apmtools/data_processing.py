e81ab1d98e0585ea05adc5c7c11e98f2a4feafdc
Federico <lorenzetti.federico@gmail.com>
2026-02-24 13:06:01 +0000

    noono

--- src/apmtools/data_processing.py
+++ src/apmtools/data_processing.py
@@ -916,9 +916,10 @@ def sum_processing(zipname,processor_name = [],return_data=False,return_csv=True
                 match i[-3:]:
                     case "csv":
                         metrics[name] = pd.read_csv(BytesIO(archive.read(i)), index_col="timestamp")
+                        metrics[name].index = metrics[name].index.map(
+                            to_datetime_metrics)
                     case ".gz":
                         metrics[name] = pd.read_json(gzip.open(BytesIO(archive.read(i)), mode="rt")).set_index("timestamp")
-                metrics[name].index = metrics[name].index.map(to_datetime_metrics)
                 to_be_dropped = list(set(metrics[name].columns).intersection(set(["channel","sensor_type_id","created_at","download_time","start_time","mission_id"])))
                 metrics[name].drop(
                     axis=1, labels=to_be_dropped, inplace=True)
